dcb1d3e93424040087dc6ff8cfd18f634472fb75
Kihong <kks4517@naver.com>
2024-04-22 19:50:25 +0900

    test: Redis 테스트코드 추가

---
+++ backend/src/test/java/oo/kr/shared/global/security/RefreshTokenServiceTest.java
@@ -0,0 +1,25 @@
+package oo.kr.shared.global.security.jwt;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import oo.kr.shared.global.exception.type.jwt.ExpiredRefreshTokenException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class RefreshTokenServiceTest {
+
+  @Autowired
+  private RefreshTokenService refreshTokenService;
+
+  @Test
+  @DisplayName("리프레시 토큰이 만료되었다면 ExpiredRefreshTokenException이 발생한다.")
+  void expiredTokenExceptionTest() {
+    assertThatThrownBy(() -> refreshTokenService.findTokenInfo("expiredToken"))
+        .isInstanceOf(ExpiredRefreshTokenException.class);
+  }
+}
